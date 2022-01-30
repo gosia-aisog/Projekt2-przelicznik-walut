79c2a6d7e368d70599d2c3123c5c2710b0f6f8d2
Bartłomiej Koczenasz <malgo.skowiera@gmail.com>
2022-01-30 21:09:29 +0100

    commit

--- js/CurrencyConverter.js
+++ js/CurrencyConverter.js
@@ -11,8 +11,23 @@ const qs = (selector) => document.querySelector(selector);
 
 //DOM Elements:
 const btnDOM = qs("#convert");
+const selectDOM = qs("#choice");
 
+//GET data:
 fetch("http://api.nbp.pl/api/exchangerates/tables/a/?format=json")
   .then((response) => response.json())
-  .then((data) => console.log(data[0].rates))
+  .then((data) => {
+    let currencies = data[0].rates;
+    console.log(currencies);
+  })
   .catch((err) => console.error(err));
+
+selectDOM.addEventListener("change", (e) => {
+  e.preventDefault();
+  const chosenCurrencyCode = e.target.value;
+  console.log(chosenCurrencyCode);
+});
+
+btnDOM.addEventListener("click", converterFunction);
+
+converterFunction = () => {};
